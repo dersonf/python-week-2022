5eb2cb0dd5e71f20270311c3b0bedbfa1406aead
Anderson <anderson@pinguim.local>
2022-05-02 21:47:30 -0300

    opção de add e list

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,42 @@
-from .config import settings
+import typer
+from typing import Optional
+from beerlog.core import add_beer_to_db, get_beers_from_db
+from rich.table import Table
+from rich.console import Console
 
 
-def main():
-    print("Hello from", settings.NAME)
+main = typer.Typer(help="Beer Management Application")
+console = Console()
+
+
+@main.command("add")
+def add(
+    # Esses precisam ser passados na ordem
+    name: str,
+    style: str,
+    # Esses já não precisam de ordem devido o nome
+    flavor: int = typer.Option(...),
+    image: int = typer.Option(...),
+    cost: int = typer.Option(...),
+):
+    """Adds a new beer to database."""
+    if add_beer_to_db(name, style, flavor, image, cost):
+        print("Beer successfully added!!!")
+    else:
+        print("Deu ruim!!!")
+
+
+@main.command("list")
+def list_beers(style: Optional[str] = None):
+    """List beers in database."""
+    beers = get_beers_from_db()
+    table = Table(title="Beerlog")
+    headers = ["id", "name", "style", "rate", "date"]
+    for header in headers:
+        table.add_column(header, style="magenta")
+    for beer in beers:
+        values = [str(getattr(beer, header)) for header in headers]
+        table.add_row(*values)
+    console.print(table)
+
+
--- beerlog/database.py
+++ beerlog/database.py
@@ -1,4 +1,9 @@
-from sqlmodel import create_engine
+from unicodedata import category
+from sqlmodel.sql.expression import Select, SelectOfScalar
+SelectOfScalar.inherit_cache = True
+Select.inherit_cache = True
+
+from sqlmodel import create_engine, Session
 from beerlog.config import settings
 from beerlog import models
 
@@ -6,3 +11,5 @@ engine = create_engine(settings.database.url)
 
 models.SQLModel.metadata.create_all(engine)
 
+def get_session():
+    return Session(engine)
