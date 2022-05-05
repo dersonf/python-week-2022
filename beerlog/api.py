75a1d0d9b75e48524fa446cc2b5e8af08266451d
Anderson <anderson79@bol.com.br>
2022-05-04 21:01:38 -0300

    finalizado API

--- beerlog/api.py
+++ beerlog/api.py
@@ -1,27 +1,29 @@
-from typing import List
+from typing import Optional, List
 from fastapi import FastAPI, Response, status
-from beerlog.core import get_beers_from_db
-from beerlog.serializers import BeerOut, BeerIn
-from beerlog.database import get_session
+from beerlog.core import get_beers_from_database
+from beerlog.serializers import BeerIn, BeerOut
 from beerlog.models import Beer
+from beerlog.database import get_session
 
-api = FastAPI(title="Beerlog")
+
+api = FastAPI(title="beerlog ")
 
 
-@api.get("/beers/", response_model=List[BeerOut])
-def list_beers():
-    beers = get_beers_from_db()
+@api.get("/beers", response_model=List[BeerOut])
+async def list_beers(style: Optional[str] = None):
+    """Lists beers from the database"""
+    beers = get_beers_from_database(style)
     return beers
 
 
-@api.post("/beers/", response_model=BeerOut)
-def add_beer(beer_in: BeerIn, response: Response):
-    print(beer_in)
+@api.post("/beers", response_model=BeerOut)
+async def add_beer(beer_in: BeerIn, response: Response):
     beer = Beer(**beer_in.dict())
     with get_session() as session:
         session.add(beer)
         session.commit()
         session.refresh(beer)
+
     response.status_code = status.HTTP_201_CREATED
     return beer
 
--- beerlog/serializers.py
+++ beerlog/serializers.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, validator
 from datetime import datetime
+from pydantic import BaseModel, validator
 from fastapi import HTTPException, status
 
 
@@ -26,8 +26,7 @@ class BeerIn(BaseModel):
         if v < 1 or v > 10:
             raise HTTPException(
                 detail=f"{field.name} must be between 1 and 10",
-                status_code=status.HTTP_400_BAD_REQUEST
-
+                status_code=status.HTTP_400_BAD_REQUEST,
             )
-            return v
+        return v
 
